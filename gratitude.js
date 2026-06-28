4372a8b7092cae4682a71af62d275a60f101aac5
saiswathiaravelli-hash <saiswathiaravelli@gmail.com>
2026-06-28 17:53:32 +0530

    Add files via upload

---
+++ gratitude.js
@@ -0,0 +1,9 @@
+let button=document.getElementById("blessingBtn");
+
+button.addEventListener("click",showBlessing);
+
+function showBlessing()
+{
+document.getElementById("message").innerHTML=
+"May Lord Krishna always guide your path with wisdom, happiness and success 🙏✨";
+}
